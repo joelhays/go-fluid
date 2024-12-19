326258938ca8f13ee056e722e205712a1cbee8d7
joelhays <joelhays@users.noreply.github.com>
2024-12-18 20:41:11 -0800

    delete inefficient fluid implementation

--- main.go
+++ main.go
@@ -24,11 +24,9 @@ const cameraZoomIncrement float32 = 0.125
 var camera = r.Camera2D{Zoom: 1.0}
 
 // simulation state
-// var fluid *f.Fluid
 var macFluid *f.MACFluid
 
 func init() {
-	// fluid = f.NewFluid(gridWidth, gridHeight)
 	macFluid = f.NewMACFluid(gridWidth)
 }
 
@@ -58,8 +56,6 @@ func main() {
 	for !r.WindowShouldClose() {
 		// handlePanAndZoom()
 		handleMouseDrag()
-
-		// fluid.Simulate(0.1) //r.GetFrameTime())
 
 		macFluid.DiffusionRate = diffusionRate
 		macFluid.Viscosity = viscosity
@@ -129,17 +125,6 @@ func handleMouseDrag() {
 	if r.IsMouseButtonDown(r.MouseButtonRight) || r.IsKeyDown(r.KeyLeftSuper) {
 		val := r.Vector2ClampValue(mouseDelta, -halfCellSize, halfCellSize)
 		macFluid.AddVelocity(x, y, val.X*force, val.Y*force)
-		// for x1 := x - int(brushRadius); x1 <= x+int(brushRadius); x1++ {
-		// 	for y1 := y - int(brushRadius); y1 <= y+int(brushRadius); y1++ {
-		// 		if int(x) == int(x1) && int(y) == int(y1) {
-		// 			continue
-		// 		}
-		// 		if int(x1) < 1 || x1 > macFluid.Size || y1 < 1 || y1 > macFluid.Size {
-		// 			continue
-		// 		}
-		// 		macFluid.AddVelocity(x1, y1, val.X, val.Y)
-		// 	}
-		// }
 	}
 	if r.IsMouseButtonDown(r.MouseButtonLeft) {
 		macFluid.AddDensity(x, y, 1)
@@ -163,8 +148,6 @@ func drawGrid() {
 	}
 
 	color := r.DarkGray
-	// for x := range gridWidth {
-	// 	for y := range gridHeight {
 	for x := range macFluid.Size + 1 {
 		for y := range macFluid.Size + 1 {
 			startX := int32(x * cellSize)
@@ -182,25 +165,8 @@ func drawVelocityField() {
 		return
 	}
 
-	// for x := range fluid.Width {
-	// 	for y := range fluid.Height {
-	// 		xv, _ := fluid.XVelocities.Get(x, y)
-	// 		yv, _ := fluid.YVelocities.Get(x, y)
-	//
-	// 		pos := r.NewVector2(float32(x*cellSize+halfCellSize), float32(y*cellSize+halfCellSize))
-	// 		dir := r.NewVector2(xv.Value, yv.Value)
-	// 		end := r.Vector2Add(pos, dir)
-	//
-	// 		r.DrawLineV(pos, end, r.Red)
-	// 		r.DrawCircleV(end, 1, r.Red)
-	// 	}
-	// }
-
-	// endSize := r.Vector2{X: 2, Y: 2}
 	for x := 1; x <= macFluid.Size; x++ {
 		for y := 1; y <= macFluid.Size; y++ {
-			// for x := range macFluid.Size + 2 {
-			// 	for y := range macFluid.Size + 2 {
 			xv := macFluid.XVelocities[x][y] * halfCellSize
 			yv := macFluid.YVelocities[x][y] * halfCellSize
 
@@ -209,41 +175,11 @@ func drawVelocityField() {
 			end := r.Vector2Add(pos, dir)
 
 			r.DrawLineV(pos, end, r.Red)
-			// if cellSize >= 16 {
-			// 	r.DrawRectangleV(end, endSize, r.Red)
-			// }
-			// r.DrawCircleV(end, 1, r.Red)
 		}
 	}
 }
 
 func drawDensityField() {
-	// for x := range fluid.Width {
-	// 	for y := range fluid.Height {
-	// 		density, _ := fluid.DensityField.Get(x, y)
-	// 		density1, _ := fluid.DensityField.Get(x+1, y)
-	// 		density2, _ := fluid.DensityField.Get(x, y+1)
-	// 		density3, _ := fluid.DensityField.Get(x+1, y+1)
-	//
-	// 		pos := r.NewVector2(float32(x*cellSize), float32(y*cellSize))
-	// 		size := r.NewVector2(cellSize, cellSize)
-	//
-	// 		baseColor := r.ColorToHSV(fluidColor)
-	// 		topLeftColor := r.ColorFromHSV(baseColor.X, baseColor.Y, baseColor.Z*density.Value)
-	// 		bottomLeftColor := r.ColorFromHSV(baseColor.X, baseColor.Y, baseColor.Z*density2.Value)
-	// 		topRightColor := r.ColorFromHSV(baseColor.X, baseColor.Y, baseColor.Z*density3.Value)
-	// 		bottomRightColor := r.ColorFromHSV(baseColor.X, baseColor.Y, baseColor.Z*density1.Value)
-	//
-	// 		r.DrawRectangleGradientEx(r.Rectangle{
-	// 			X:      pos.X,
-	// 			Y:      pos.Y,
-	// 			Width:  size.X,
-	// 			Height: size.Y,
-	// 		}, topLeftColor, bottomLeftColor, topRightColor, bottomRightColor)
-	// 	}
-	// }
-	// for x := 1; x <= macFluid.Size; x++ {
-	// 	for y := 1; y <= macFluid.Size; y++ {
 	for x := range macFluid.Size + 1 {
 		for y := range macFluid.Size + 1 {
 			density := macFluid.DensityField[x][y]
